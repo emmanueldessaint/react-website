fef390c729189be86a10ab7c9332ab552f92ed47
Mathieu Dessaint <mathieu.dessaint10@gmail.com>
2021-10-28 20:12:22 +0200

    orders, orders products

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -4,8 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\OrderProduct;
+use App\Models\User;
 
 class Order extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        
+    ];
+
+    public function orderProducts() {
+        return $this->hasMany(OrderProduct::class, 'id_order');
+    }
+
+    public function order() {
+        return $this->belongsTo(User::class, 'id_user');
+    }
 }
--- database/migrations/2021_10_15_110137_create_orders_table.php
+++ database/migrations/2021_10_15_110137_create_orders_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateOrderTable extends Migration
+class CreateOrdersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,9 +13,11 @@ class CreateOrderTable extends Migration
      */
     public function up()
     {
-        Schema::create('order', function (Blueprint $table) {
+        Schema::create('orders', function (Blueprint $table) {
             $table->id();
-            $table->unsignedInteger('id_order');
+            $table->unsignedInteger('id_customer_order');
+            $table->unsignedBigInteger('id_user');
+            $table->foreign('id_user')->references('id')->on('users');
             $table->string('payer_firstname');
             $table->string('payer_lastname');
             $table->string('payer_email');
