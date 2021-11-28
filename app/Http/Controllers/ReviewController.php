2d036c88ce84c2f9665a1bb380284089cc749f57
Mathieu Dessaint <mathieu.dessaint10@gmail.com>
2021-11-28 19:33:42 +0100

    review backend

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -7,11 +7,20 @@ use App\Models\ProductReviews;
 
 class ReviewController extends Controller
 {
-    public function createReview(Request $request) {
-        dd($request);
-        // $review = ProductReview::create([
-        //     // 'id_product' => $request->
-        // ]);
-        // $review->save();
+    public function create(Request $request) {
+        $review = ProductReviews::create([
+            'id_product' => $request->newComment['id_product'],
+            'id_user' => 1,
+            // 'id_user' => $request->newComment['id_user'],
+            'note' => $request->newComment['note'],
+            'title' => $request->newComment['title'],
+            'description' => $request->newComment['description'],
+            'date' => now(),
+        ]);
+        $review->save();
+        
+        return json_encode([
+            "success" => 'Review published!'
+        ]);
     }
 }
